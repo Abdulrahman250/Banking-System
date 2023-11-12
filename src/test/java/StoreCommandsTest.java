761fa973cd6cc15dcb0a8382050d9f2e571e7aff
Abdulrahman Alsaidi <aya49@drexel.edu>
2023-11-12 00:42:38 -0500

    I finished part 2 of assignment 6, which was creating the storeCommands.

---
+++ src/test/java/StoreCommandsTest.java
@@ -0,0 +1,37 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class StoreCommandsTest {
+	StoreCommands storeCommands;
+
+	@BeforeEach
+	public void setUp() {
+		storeCommands = new StoreCommands();
+	}
+
+	@Test
+	public void check_if_there_is_no_invalid_commands() {
+		assertTrue(storeCommands.getInvalidCommands().isEmpty());
+	}
+
+	@Test
+	public void add_invalid_commands() {
+		storeCommands.addInvalidCommands("Invalid command");
+
+		assertEquals(1, storeCommands.getInvalidCommands().size());
+
+	}
+
+	@Test
+	public void add_two_invalid_commands() {
+		storeCommands.addInvalidCommands("Invalid command");
+		storeCommands.addInvalidCommands("Other invalid command");
+
+		assertEquals(2, storeCommands.getInvalidCommands().size());
+		assertTrue(storeCommands.getInvalidCommands().contains("Invalid command"));
+		assertTrue(storeCommands.getInvalidCommands().contains("Other invalid command"));
+	}
+}
